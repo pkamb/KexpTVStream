29da2f21a9c6331ce3637fb72973f1408e8b3ba8
Dustin Bergman <dustin.bergman@gmail.com>
2016-01-06 22:09:29 -0500

    Added InternetReachability Class to confirm internet is present.

---
+++ KexpTVStream/KexpTVStream/InternetReachability.swift
@@ -0,0 +1,32 @@
+//
+//  InternetReachability.swift
+//  KexpTVStream
+//
+//  Created by Dustin Bergman on 1/6/16.
+//  Copyright © 2016 Dustin Bergman. All rights reserved.
+//
+
+import SystemConfiguration
+
+public class InternetReachability {
+    class func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in()
+        
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+        }
+        var flags = SCNetworkReachabilityFlags()
+        
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        
+        return (isReachable && !needsConnection)
+    }
+}
