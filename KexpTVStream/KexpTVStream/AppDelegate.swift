540c8fd29b7198a64721ddc5049d6748044cc813
Dustin Bergman <dustin.bergman@gmail.com>
2016-01-06 21:37:36 -0500

    Added feature that keeps app from sleeping

--- KexpTVStream/KexpTVStream/AppDelegate.swift
+++ KexpTVStream/KexpTVStream/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        UIApplication.sharedApplication().idleTimerDisabled = true
+        
         return true
     }
 
