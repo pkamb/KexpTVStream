60d68a1a0952b9568a8cd3b1bc10c25c52fad96c
Dustin Bergman <dustin.bergman@gmail.com>
2016-01-17 18:42:43 -0500

    Populated KEXP Config settings unit test

--- KexpTVStream/KexpTVStreamTests/KexpConfigResponseUnitTest.swift
+++ KexpTVStream/KexpTVStreamTests/KexpConfigResponseUnitTest.swift
@@ -9,27 +9,22 @@
 import XCTest
 
 class KexpConfigResponseUnitTest: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    lazy var configSettings: KexpConfigSettings = self.getKexpSettings()
+
+    func testConfigResponse() {
+        XCTAssertNotNil(configSettings);
+
+        XCTAssert(configSettings.streamUrl == "http://live-aacplus-64.kexp.org/kexp64.aac", "Kexp Stream URL not present")
+        XCTAssert(configSettings.backupStreamUrl == "http://live-mp3-128.kexp.org:8000/listen.pls", "Back up Kexp Stream URL not present")
+        XCTAssert(configSettings.nowPlayingLogo?.characters.count > 0, "Now playing logo is not present")
+        XCTAssert(configSettings.updated > 0, "Updated timestamp is not present")
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func getKexpSettings() -> KexpConfigSettings {
+        let jsonDictionary = KexpTestUtilities.getJSONFromTestFile("KexpConfigResponse")
+        let kexpConfigResponse =  KexpConfigSettings(configSettingDictionary: jsonDictionary)
+        
+        return kexpConfigResponse
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
