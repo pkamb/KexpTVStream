48147f481a6d3a9c142a973cc1897ce37c75bc78
Dustin Bergman <dustin.bergman@gmail.com>
2016-06-05 16:47:21 -0700

    Updated UI for playlist cell

--- KexpTVStream/KexpTVStream/KexpPlaylistCell.swift
+++ KexpTVStream/KexpTVStream/KexpPlaylistCell.swift
@@ -9,38 +9,141 @@
 import Foundation
 import UIKit
 
+private let albumArtSize:CGFloat = 100.0
+private let bottomBorderHeight:CGFloat = 1.0
+
 class KexpPlaylistCell: UITableViewCell {
     
-    let artistLabel = UILabel()
-    let trackLabel = UILabel()
-    let albumLabel = UILabel()
-    let albumArtImageView = UIImageView()
+    private var artistLabel: UILabel = {
+        let artistLabel = UILabel()
+        artistLabel.translatesAutoresizingMaskIntoConstraints = false
+        artistLabel.font = UIFont.boldSystemFontOfSize(FontSizes.xsmall)
+        artistLabel.numberOfLines = 1
+        return artistLabel
+    }()
+    
+    private var trackLabel: UILabel = {
+        let trackLabel = UILabel()
+        trackLabel.translatesAutoresizingMaskIntoConstraints = false
+        trackLabel.font = UIFont.italicSystemFontOfSize(FontSizes.xsmall)
+        trackLabel.numberOfLines = 1
+        return trackLabel
+    }()
+    
+    private var albumLabel: UILabel = {
+        let albumLabel = UILabel()
+        albumLabel.translatesAutoresizingMaskIntoConstraints = false
+        albumLabel.font = UIFont.systemFontOfSize(FontSizes.xsmall)
+        albumLabel.numberOfLines = 1
+        albumLabel.textColor = UIColor.grayColor()
+        return albumLabel
+    }()
+    
+    private var timePlayedLabel: UILabel = {
+        let timePlayedLabel = UILabel()
+        timePlayedLabel.translatesAutoresizingMaskIntoConstraints = false
+        timePlayedLabel.font = UIFont.systemFontOfSize(FontSizes.xxsmall)
+        timePlayedLabel.numberOfLines = 1
+        return timePlayedLabel
+    }()
+    
+    private let dateFormatter: NSDateFormatter = {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "hh:mma MM/dd/YYYY"
+        dateFormatter.timeZone = NSTimeZone.localTimeZone()
+        return dateFormatter
+    }()
+
+    private let albumArtImageView = UIImageView()
 
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         selectionStyle = .None
         
+        let bottomBorder = UIView()
+        bottomBorder.translatesAutoresizingMaskIntoConstraints = false
+        albumArtImageView.translatesAutoresizingMaskIntoConstraints = false
+        bottomBorder.backgroundColor = UIColor.grayColor()
         
-        artistLabel.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(artistLabel)
-        
-        let views = ["settingsLabel": artistLabel]
+        contentView.addSubview(trackLabel)
+        contentView.addSubview(albumArtImageView)
+        contentView.addSubview(albumLabel)
+        contentView.addSubview(timePlayedLabel)
+        contentView.addSubview(bottomBorder)
+
+        let views = ["artistLabel": artistLabel, "albumArtImageView": albumArtImageView, "trackLabel": trackLabel, "albumLabel": albumLabel, "timePlayedLabel": timePlayedLabel, "bottomBorder": bottomBorder]
+        let metrics = ["albumArtSize": albumArtSize, "bottomBorderHeight": bottomBorderHeight]
         
         NSLayoutConstraint.activateConstraints(
             NSLayoutConstraint.constraintsWithVisualFormat(
-                "H:|[settingsLabel]-|",
-                options: .AlignAllCenterY,
-                metrics:nil,
+                "H:|-[albumArtImageView(albumArtSize)]-[artistLabel]-|",
+                options: [],
+                metrics:metrics,
                 views: views
             )
         )
         
         NSLayoutConstraint.activateConstraints(
             NSLayoutConstraint.constraintsWithVisualFormat(
-                "V:|-mMargin-[settingsLabel]-mMargin-|",
+                "H:|-[albumArtImageView(albumArtSize)]-[trackLabel]-|",
                 options: [],
-                metrics:nil,
+                metrics:metrics,
+                views: views
+            )
+        )
+        
+        NSLayoutConstraint.activateConstraints(
+            NSLayoutConstraint.constraintsWithVisualFormat(
+                "H:|-[albumArtImageView(albumArtSize)]-[albumLabel]-|",
+                options: [],
+                metrics:metrics,
+                views: views
+            )
+        )
+        
+        NSLayoutConstraint.activateConstraints(
+            NSLayoutConstraint.constraintsWithVisualFormat(
+                "H:|-[albumArtImageView(albumArtSize)]-[timePlayedLabel]-|",
+                options: [],
+                metrics:metrics,
+                views: views
+            )
+        )
+        
+        NSLayoutConstraint.activateConstraints(
+            NSLayoutConstraint.constraintsWithVisualFormat(
+                "H:|[bottomBorder]|",
+                options: [],
+                metrics:metrics,
+                views: views
+            )
+        )
+        
+        NSLayoutConstraint.activateConstraints(
+            NSLayoutConstraint.constraintsWithVisualFormat(
+                "V:|-[albumArtImageView(albumArtSize)]-|",
+                options: [],
+                metrics:metrics,
+                views: views
+            )
+        )
+        
+        NSLayoutConstraint.activateConstraints(
+            NSLayoutConstraint.constraintsWithVisualFormat(
+                "V:|-[artistLabel][trackLabel][albumLabel][timePlayedLabel]",
+                options: [],
+                metrics:metrics,
+                views: views
+            )
+        )
+        
+        NSLayoutConstraint.activateConstraints(
+            NSLayoutConstraint.constraintsWithVisualFormat(
+                "V:[bottomBorder(bottomBorderHeight)]|",
+                options: [],
+                metrics:metrics,
                 views: views
             )
         )
@@ -48,5 +151,20 @@ class KexpPlaylistCell: UITableViewCell {
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        assertionFailure("Not implemented!")
+    }
+    
+    func configureNowPlayingCell(song: NowPlaying) {
+        artistLabel.text = song.artist
+        trackLabel.text = song.songTitle
+        albumLabel.text = song.album
+        timePlayedLabel.text = dateFormatter.stringFromDate(song.timePlayed)
+        albumArtImageView.image = UIImage(named: "vinylPlaceHolder")
+    
+        if let albumURLString = song.albumArtWork as String? {
+            if let albumURL = NSURL.init(string: albumURLString) {
+                albumArtImageView.af_setImageWithURL(albumURL)
+            }
+        }
     }
 }
